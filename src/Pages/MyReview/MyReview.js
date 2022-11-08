0d3ca736c4b5c5d2412826e8201e818fe25e9d50
didar07 <didarul111015@gmail.com>
2022-11-09 00:51:42 +0600

    review delete added

--- src/Pages/MyReview/MyReview.js
+++ src/Pages/MyReview/MyReview.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 import MyReviewRow from './MyReviewRow';
 
@@ -12,6 +13,24 @@ const MyReview = () => {
             .then(data => setReview(data))
     }, [user?.email])
 
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure you want to delete?')
+        if (proceed) {
+            fetch(`http://localhost:5000/myreviews/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        toast.success('deleted succefully')
+                        const remaining = review.filter(rvw => rvw._id !== id)
+                        setReview(remaining)
+                    }
+                })
+        }
+    }
 
     return (
         <div>
@@ -31,11 +50,23 @@ const MyReview = () => {
                         review.map(rvwRow => <MyReviewRow
                             key={rvwRow._id}
                             rvwRow={rvwRow}
+                            handleDelete={handleDelete}
                         ></MyReviewRow>)
                     }
                 </tbody>
             </table>
-
+            <ToastContainer
+                position="top-center"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            ></ToastContainer>
         </div>
 
     );
--- src/Pages/MyReview/MyReviewRow.js
+++ src/Pages/MyReview/MyReviewRow.js
@@ -1,47 +1,55 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
-const MyReviewRow = ({ rvwRow }) => {
+const MyReviewRow = ({ rvwRow, handleDelete }) => {
     const { user } = useContext(AuthContext)
-    const { reviewName, customer, img, message } = rvwRow
-    return (
-        <tr>
-            <th>
-                <button>X</button>
-            </th>
-            <td>
-                <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                        <div className="rounded w-24 h-24">
-                            {
-                                user?.img ?
-                                    <img src={img} alt="" />
-                                    :
-                                    <img src="https://tse4.mm.bing.net/th?id=OIP.DZLWFqYqIG4l_yJaqOuJXgHaHa&pid=Api&P=0" alt="" />
-                            }
+    const { _id, reviewName, customer, img, message } = rvwRow
 
+
+    if (rvwRow.length === 0) {
+        return <h1 className='text-4xl font-bold justify-center items-center'>No reviews were added</h1>
+    }
+
+    else {
+        return (
+            <tr>
+                <th>
+                    <button onClick={() => handleDelete(_id)} title='delete this review' className='btn btn-outline btn-ghost'>X</button>
+                </th>
+                <td>
+                    <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                            <div className="rounded w-24 h-24">
+                                {
+                                    user?.img ?
+                                        <img src={img} alt="" />
+                                        :
+                                        <img src="https://tse4.mm.bing.net/th?id=OIP.DZLWFqYqIG4l_yJaqOuJXgHaHa&pid=Api&P=0" alt="" />
+                                }
+
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-bold">{reviewName}</div>
+                            <div className="text-sm opacity-50"></div>
                         </div>
                     </div>
-                    <div>
-                        <div className="font-bold">{reviewName}</div>
-                        <div className="text-sm opacity-50"></div>
-                    </div>
-                </div>
-            </td>
-            <td>
-                {customer}
-                <br />
-                <span className="badge badge-ghost badge-sm"></span>
-            </td>
-            <td>{message}</td>
-            <th>
-                <button
-                    className="btn btn-ghost btn-xs"></button>
-            </th>
-        </tr>
+                </td>
+                <td>
+                    {customer}
+                    <br />
+                    <span className="badge badge-ghost badge-sm"></span>
+                </td>
+                <td>{message}</td>
+                <th>
+                    <button
+                        className="btn btn-ghost btn-xs"></button>
+                </th>
+            </tr>
 
 
-    );
+        );
+    }
 };
 
 export default MyReviewRow;
