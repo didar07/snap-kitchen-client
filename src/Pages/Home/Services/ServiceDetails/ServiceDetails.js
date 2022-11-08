a0878ac8893ee224b1b9746d18e2e34f6858f493
didar07 <didarul111015@gmail.com>
2022-11-08 16:21:44 +0600

    service detail added

--- src/Pages/Home/Services/ServiceDetails/ServiceDetails.js
+++ src/Pages/Home/Services/ServiceDetails/ServiceDetails.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
+    const { title, description, price, img } = useLoaderData()
     return (
         <div>
+            <h1 className='text-5xl w-1/2 text-center font-bold bg-orange-500 px-4 py-3 rounded-lg'>Servive Section</h1>
+            <div>
+                <h2 className='text-4xl mt-5 mb-6 font-bold'>{title}</h2>
 
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src={img} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{title}</h2>
+                        <p>price: $ {price}</p>
+                        <p>{description}</p>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -22,7 +22,8 @@ const router = createBrowserRouter([
             },
             {
                 path: 'servicedetails/:id',
-                element: <ServiceDetails></ServiceDetails>
+                element: <ServiceDetails></ServiceDetails>,
+                loader: ({ params }) => fetch(`http://localhost:5000/allservice/${params.id}`)
             },
             {
                 path: '/login',
